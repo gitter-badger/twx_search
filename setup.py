a899bb6e6cb749cfd8f1dc259fcf1ec1eed973b5
Vivek Gangadharan <vivek.gangadharan@cognizant.com>
2017-09-09 19:45:01 +0100

    ui changes
    Signed-off-by: Vivek Gangadharan <vivek.gangadharan@cognizant.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='twx_search',
+    version='0.1',
+    packages=[],
+    url='',
+    license='',
+    author='vivek',
+    author_email='',
+    description='', requires=['flask', 'whoosh']
+)
